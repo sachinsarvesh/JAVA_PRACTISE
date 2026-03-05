db102f0e76085a0ad3b9ba5a67f56505d048ede3
Sarvy <sarvesh240698@gmail.com>
2026-03-05 17:14:50 +0530

    MultiplyTwoFloat

---
+++ src/BasicPrograms/MultiplyTwoFloat.java
@@ -0,0 +1,14 @@
+package BasicPrograms;
+
+public class MultiplyTwoFloat {
+
+    public static void main(String[] args){
+        float f1 = 1.5f;
+        float f2 = 2.5f;
+
+        float multiplied_value = f1 * f2;
+
+        System.out.println("Product of " + f1 + " and " + f2 + " is " + multiplied_value);
+
+    }
+}
