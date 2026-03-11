893d329ad9a4f4160bcbb59c45e22f79c0588f51
Sarvy <sarvesh240698@gmail.com>
2026-03-11 12:04:42 +0530

    Diff Solution to reverse a string

--- src/Strings/StringReverse.java
+++ src/Strings/StringReverse.java
@@ -1,4 +1,38 @@
 package Strings;
 
 public class StringReverse {
+    public static void main(String[] args){
+        String actualString = "SACHINSARVESH";
+
+        //Method 1 - inbuilt method
+        String reveredString = new StringBuilder(actualString).reverse().toString();
+        System.out.println("Method 1 - inbuilt method - " + reveredString);
+        //System.out.println("Actual String : " + actualString);
+        //System.out.println("Reversed String : " + reveredString);
+
+        //Method 2 - looping
+        String reversedViaLoop ="";
+        for (int i = actualString.length() -1; i>=0 ; i--){
+            reversedViaLoop += actualString.charAt(i);
+        }
+        System.out.println("Method 2 - looping method - " +reversedViaLoop);
+        //System.out.println("Actual String : " + actualString);
+        //System.out.println("Reversed String : " + reversedViaLoop);
+
+        //Method 3 - Two Pointer
+        char[] chars = actualString.toCharArray();
+        int left = 0;
+        int right = actualString.length()-1;
+
+        while(left < right){
+            char temp = chars[left];
+            chars[left++] = chars[right];
+            chars[right--] = temp;
+            //left++;
+            //right--;
+        }
+        String reveredViaTP = new String(chars);
+        System.out.println("Method 3 - Two pointer method : " + reveredViaTP);
+
+    }
 }
