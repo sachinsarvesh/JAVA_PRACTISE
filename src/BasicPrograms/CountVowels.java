89c82bfa716f2540546d938644a43641681c6ab7
Sarvy <sarvesh240698@gmail.com>
2026-03-11 12:02:48 +0530

    To count vowels in an input string

--- src/BasicPrograms/CountVowels.java
+++ src/BasicPrograms/CountVowels.java
@@ -1,4 +1,15 @@
 package BasicPrograms;
 
 public class CountVowels {
+    public static void main(String[] args){
+        String text = "seleniumautomation";
+        int count = 0;
+        for(int i=0; i<text.length();i++){
+            char ch = text.charAt(i);
+            if(ch =='a' || ch =='e' || ch =='i' || ch =='o' || ch =='u'){
+                count++;
+            }
+        }
+        System.out.println("Total no of vowels in text " + text + " is " + count);
+    }
 }
