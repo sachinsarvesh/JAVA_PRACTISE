8767176513e52aea42ffa223de41666330fa7e4d
Sarvy <sarvesh240698@gmail.com>
2026-03-11 12:03:58 +0530

    To count No of char in a string

--- src/Strings/CountNumOfChar.java
+++ src/Strings/CountNumOfChar.java
@@ -1,4 +1,12 @@
 package Strings;
 
+import java.util.Scanner;
+
 public class CountNumOfChar {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the word to count the length");
+        String word = scanner.nextLine();
+        System.out.println("The count of word - \"" + word +"\" is " + word.length());
+    }
 }
