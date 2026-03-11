207ac35073230c5dee5037f31117358b931a5804
Sarvy <sarvesh240698@gmail.com>
2026-03-11 12:03:21 +0530

    To find sum of an array

--- src/BasicPrograms/SumOfAnArray.java
+++ src/BasicPrograms/SumOfAnArray.java
@@ -1,4 +1,17 @@
 package BasicPrograms;
 
 public class SumOfAnArray {
+    public static void main(String[] args){
+        int[] numbers = {5,10,15,20,25};
+        int sum = 0;
+
+//        for(int i =0; i < numbers.length; i++){
+//            sum += numbers[i];
+//        }
+        for(int num : numbers){
+            sum += num;
+        }
+
+        System.out.println("Sum of array is " + sum);
+    }
 }
